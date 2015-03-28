820656a2f021d07ff0757df9a5992aee098918af
Sankar சங்கர் <sankar.curiosity@gmail.com>
2015-03-28 22:39:52 +0530

    move table related code to a new file

--- keeri/keeri.go
+++ keeri/keeri.go
@@ -10,68 +10,11 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
-	"sync"
 )
 
 // The database descriptor that could hold any number of tables
 type Keeri struct {
 	tables map[string]*table
-}
-
-type table struct {
-	cols     columnList
-	colsDesc []ColumnDesc
-
-	rowCounter     rowID
-	rowCounterLock sync.RWMutex
-}
-
-func (t *table) newRowID() rowID {
-	// We could use UNIXTIME EPOCH as the rowid
-	// instead of maintaining a counter, if there
-	// are guarantees that the clock will not be changed
-	// to a past time, after the db+process is started
-	t.rowCounterLock.Lock()
-	defer t.rowCounterLock.Unlock()
-
-	t.rowCounter += 1
-	return t.rowCounter
-}
-
-func (t *table) curRowID() rowID {
-	t.rowCounterLock.RLock()
-	defer t.rowCounterLock.RUnlock()
-	return t.rowCounter
-}
-
-func (t *table) String() string {
-	s := "\n\n"
-
-	for i, j := range t.colsDesc {
-		s += fmt.Sprintf("%d) %s:%d\n", i, j.ColName, j.ColType)
-	}
-
-	s += "\nrowCounter: "
-	s += strconv.Itoa(int(t.curRowID()))
-	s += "\n"
-	s += fmt.Sprintf("%v", t.cols)
-	s += "\n\n"
-	return s
-}
-
-// All possible datatypes for the columns
-type ColumnType int
-
-const (
-	IntColumn ColumnType = iota
-	StringColumn
-	CustomColumn
-)
-
-// Conveys the Name and the Type of any column
-type ColumnDesc struct {
-	ColName string
-	ColType ColumnType
 }
 
 // maps column name to column pointer
